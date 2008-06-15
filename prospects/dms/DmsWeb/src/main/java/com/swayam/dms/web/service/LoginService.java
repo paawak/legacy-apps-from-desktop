6dbc5d4666e69b58f9a5902348040a8ca66420de
paawak <paawak@gmail.com>
2008-06-15 13:52:53 +0000

    git-svn-id: svn://localhost/swayam@25 c64e1095-0faf-4994-8903-70d657f34d2f

---
+++ prospects/dms/DmsWeb/src/main/java/com/swayam/dms/web/service/LoginService.java
@@ -0,0 +1,24 @@
+/*
+ * LoginService.java
+ *
+ * Created on Jun 15, 2008 5:40:40 PM
+ *
+ * Copyright (c) 2002 - 2008 : Swayam Inc.
+ *
+ * P R O P R I E T A R Y & C O N F I D E N T I A L
+ *
+ * The copyright of this document is vested in Swayam Inc. without
+ * whose prior written permission its contents must not be published,
+ * adapted or reproduced in any form or disclosed or
+ * issued to any third party.
+ */
+
+package com.swayam.dms.web.service;
+
+/**
+ *  
+ * @author paawak
+ */
+public interface LoginService {
+
+}

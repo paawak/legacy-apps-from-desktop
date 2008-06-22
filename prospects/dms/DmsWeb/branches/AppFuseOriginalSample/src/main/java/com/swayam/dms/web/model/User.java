456e4cc20ba8a431ca564ec9c270edb8edf66d06
paawak <paawak@gmail.com>
2008-06-22 18:52:19 +0000

    renamed db tables
    git-svn-id: svn://localhost/swayam@77 c64e1095-0faf-4994-8903-70d657f34d2f

--- prospects/dms/DmsWeb/branches/AppFuseOriginalSample/src/main/java/com/swayam/dms/web/model/User.java
+++ prospects/dms/DmsWeb/branches/AppFuseOriginalSample/src/main/java/com/swayam/dms/web/model/User.java
@@ -1,39 +1,52 @@
 package com.swayam.dms.web.model;
 
-import org.springframework.security.GrantedAuthority;
-import org.springframework.security.userdetails.UserDetails;
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
-
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.userdetails.UserDetails;
+
 /**
- * This class represents the basic "user" object in AppFuse that allows for authentication
- * and user management.  It implements Acegi Security's UserDetails interface.
- *
- * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
- *         Updated by Dan Kibler (dan@getrolling.com)
- *         Extended to implement Acegi UserDetails interface
- *         by David Carter david@carter.net
+ * This class represents the basic "user" object in AppFuse that allows for
+ * authentication and user management. It implements Acegi Security's
+ * UserDetails interface.
+ * 
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a> Updated by
+ *         Dan Kibler (dan@getrolling.com) Extended to implement Acegi
+ *         UserDetails interface by David Carter david@carter.net
  */
 @Entity
-@Table(name="app_user")
+@Table(name = "employee")
 public class User extends BaseObject implements Serializable, UserDetails {
     private static final long serialVersionUID = 3832626162173359411L;
 
     private Long id;
-    private String username;                    // required
-    private String password;                    // required
+    private String username; // required
+    private String password; // required
     private String confirmPassword;
     private String passwordHint;
-    private String firstName;                   // required
-    private String lastName;                    // required
-    private String email;                       // required; unique
+    private String firstName; // required
+    private String lastName; // required
+    private String email; // required; unique
     private String phoneNumber;
     private String website;
     private Address address = new Address();
@@ -47,27 +60,31 @@ public class User extends BaseObject implements Serializable, UserDetails {
     /**
      * Default constructor - creates a new instance with no values set.
      */
-    public User() {}
+    public User() {
+    }
 
     /**
      * Create a new instance and set the username.
-     * @param username login name for user.
+     * 
+     * @param username
+     *            login name for user.
      */
     public User(final String username) {
         this.username = username;
     }
 
-    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public Long getId() {
         return id;
     }
 
-    @Column(nullable=false,length=50,unique=true)
+    @Column(nullable = false, length = 50, unique = true)
     public String getUsername() {
         return username;
     }
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     public String getPassword() {
         return password;
     }
@@ -77,27 +94,27 @@ public class User extends BaseObject implements Serializable, UserDetails {
         return confirmPassword;
     }
 
-    @Column(name="password_hint")
+    @Column(name = "password_hint")
     public String getPasswordHint() {
         return passwordHint;
     }
 
-    @Column(name="first_name",nullable=false,length=50)
+    @Column(name = "first_name", nullable = false, length = 50)
     public String getFirstName() {
         return firstName;
     }
 
-    @Column(name="last_name",nullable=false,length=50)
+    @Column(name = "last_name", nullable = false, length = 50)
     public String getLastName() {
         return lastName;
     }
 
-    @Column(nullable=false,unique=true)
+    @Column(nullable = false, unique = true)
     public String getEmail() {
         return email;
     }
 
-    @Column(name="phone_number")
+    @Column(name = "phone_number")
     public String getPhoneNumber() {
         return phoneNumber;
     }
@@ -108,6 +125,7 @@ public class User extends BaseObject implements Serializable, UserDetails {
 
     /**
      * Returns the full name.
+     * 
      * @return firstName + ' ' + lastName
      */
     @Transient
@@ -120,18 +138,15 @@ public class User extends BaseObject implements Serializable, UserDetails {
         return address;
     }
 
-    @ManyToMany(fetch = FetchType.EAGER) 
-    @JoinTable(
-            name="user_role",
-            joinColumns = { @JoinColumn( name="user_id") },
-            inverseJoinColumns = @JoinColumn( name="role_id")
-    )    
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "employee_role", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = @JoinColumn(name = "role_id"))
     public Set<Role> getRoles() {
         return roles;
     }
 
     /**
      * Convert user roles to LabelValue objects for convenience.
+     * 
      * @return a list of LabelValue objects with role information
      */
     @Transient
@@ -150,12 +165,14 @@ public class User extends BaseObject implements Serializable, UserDetails {
 
     /**
      * Adds a role for the user
-     * @param role the fully instantiated role
+     * 
+     * @param role
+     *            the fully instantiated role
      */
     public void addRole(Role role) {
         getRoles().add(role);
     }
-    
+
     /**
      * @see org.springframework.security.userdetails.UserDetails#getAuthorities()
      * @return GrantedAuthority[] an array of roles.
@@ -169,17 +186,17 @@ public class User extends BaseObject implements Serializable, UserDetails {
     public Integer getVersion() {
         return version;
     }
-    
-    @Column(name="account_enabled")
+
+    @Column(name = "account_enabled")
     public boolean isEnabled() {
         return enabled;
     }
-    
-    @Column(name="account_expired",nullable=false)
+
+    @Column(name = "account_expired", nullable = false)
     public boolean isAccountExpired() {
         return accountExpired;
     }
-    
+
     /**
      * @see org.springframework.security.userdetails.UserDetails#isAccountNonExpired()
      */
@@ -188,11 +205,11 @@ public class User extends BaseObject implements Serializable, UserDetails {
         return !isAccountExpired();
     }
 
-    @Column(name="account_locked",nullable=false)
+    @Column(name = "account_locked", nullable = false)
     public boolean isAccountLocked() {
         return accountLocked;
     }
-    
+
     /**
      * @see org.springframework.security.userdetails.UserDetails#isAccountNonLocked()
      */
@@ -201,11 +218,11 @@ public class User extends BaseObject implements Serializable, UserDetails {
         return !isAccountLocked();
     }
 
-    @Column(name="credentials_expired",nullable=false)
+    @Column(name = "credentials_expired", nullable = false)
     public boolean isCredentialsExpired() {
         return credentialsExpired;
     }
-    
+
     /**
      * @see org.springframework.security.userdetails.UserDetails#isCredentialsNonExpired()
      */
@@ -213,11 +230,11 @@ public class User extends BaseObject implements Serializable, UserDetails {
     public boolean isCredentialsNonExpired() {
         return !credentialsExpired;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
-    
+
     public void setUsername(String username) {
         this.username = username;
     }
@@ -265,7 +282,7 @@ public class User extends BaseObject implements Serializable, UserDetails {
     public void setVersion(Integer version) {
         this.version = version;
     }
-    
+
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
@@ -273,7 +290,7 @@ public class User extends BaseObject implements Serializable, UserDetails {
     public void setAccountExpired(boolean accountExpired) {
         this.accountExpired = accountExpired;
     }
-    
+
     public void setAccountLocked(boolean accountLocked) {
         this.accountLocked = accountLocked;
     }
@@ -281,7 +298,7 @@ public class User extends BaseObject implements Serializable, UserDetails {
     public void setCredentialsExpired(boolean credentialsExpired) {
         this.credentialsExpired = credentialsExpired;
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -295,7 +312,8 @@ public class User extends BaseObject implements Serializable, UserDetails {
 
         final User user = (User) o;
 
-        return !(username != null ? !username.equals(user.getUsername()) : user.getUsername() != null);
+        return !(username != null ? !username.equals(user.getUsername()) : user
+                .getUsername() != null);
 
     }
 
@@ -310,12 +328,12 @@ public class User extends BaseObject implements Serializable, UserDetails {
      * {@inheritDoc}
      */
     public String toString() {
-        ToStringBuilder sb = new ToStringBuilder(this, ToStringStyle.DEFAULT_STYLE)
-                .append("username", this.username)
-                .append("enabled", this.enabled)
-                .append("accountExpired", this.accountExpired)
-                .append("credentialsExpired", this.credentialsExpired)
-                .append("accountLocked", this.accountLocked);
+        ToStringBuilder sb = new ToStringBuilder(this,
+                ToStringStyle.DEFAULT_STYLE).append("username", this.username)
+                .append("enabled", this.enabled).append("accountExpired",
+                        this.accountExpired).append("credentialsExpired",
+                        this.credentialsExpired).append("accountLocked",
+                        this.accountLocked);
 
         GrantedAuthority[] auths = this.getAuthorities();
         if (auths != null) {

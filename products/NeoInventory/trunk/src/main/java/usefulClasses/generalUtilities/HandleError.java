bbe8b6598b069775e9911d076eda5eb3233535a9
paawak <paawak@gmail.com>
2008-12-17 16:51:51 +0000

    display stack trace
    git-svn-id: svn://localhost/swayam@179 c64e1095-0faf-4994-8903-70d657f34d2f

--- products/NeoInventory/trunk/src/main/java/usefulClasses/generalUtilities/HandleError.java
+++ products/NeoInventory/trunk/src/main/java/usefulClasses/generalUtilities/HandleError.java
@@ -11,19 +11,61 @@ package usefulClasses.generalUtilities;
  * @author  Administrator
  */
 
-import javax.swing.JOptionPane;
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-public class HandleError extends javax.swing.JFrame {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import view.InventoryFrame;
+
+public class HandleError extends javax.swing.JDialog {
+	
+	private JLabel errorMessage;
+	private JTextArea stackTrace;
     
     /** Creates a new instance of HandleError */
     public HandleError() {
-            }//end constructor
+    	getContentPane().setLayout(new BorderLayout());
+    	errorMessage = new JLabel();
+    	stackTrace = new JTextArea();
+    	JScrollPane scroll = new JScrollPane();
+    	scroll.setViewportView(stackTrace);
+    	getContentPane().add(errorMessage, BorderLayout.NORTH);
+    	getContentPane().add(scroll, BorderLayout.CENTER);
+    	
+    	addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                
+            }
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                setVisible(false);
+            }
+        });
+    	
+    	setSize(600, 200);
+    	
+    }//end constructor
 
-    public void displayError(String invokingMethodName,Exception exception){
+    public void displayError(String invokingMethodName,Exception e){
+        setTitle("An ERROR has occured in "+invokingMethodName);
+        errorMessage.setText(e.getMessage()+"\nERROR DETAILS:");
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        e.printStackTrace(new PrintStream(bos));
+        String trace = new String(bos.toByteArray());
+        stackTrace.setText(trace);
         
-            JOptionPane.showMessageDialog(this,"ERROR DETAILS :\n"+exception,"An ERROR has occured in "+invokingMethodName+":               ",JOptionPane.ERROR_MESSAGE);
-
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+            	setVisible(true);
+            }
+        });
+        
     }//end methd.
-    
     
 }//end class

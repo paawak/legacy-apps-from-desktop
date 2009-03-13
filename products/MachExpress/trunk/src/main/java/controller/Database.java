3ae02d11043e507e294cb7b8c83e68174df995c8
paawak <paawak@gmail.com>
2009-03-13 17:22:37 +0000

    git-svn-id: svn://localhost/swayam@190 c64e1095-0faf-4994-8903-70d657f34d2f

--- products/MachExpress/trunk/src/main/java/controller/Database.java
+++ products/MachExpress/trunk/src/main/java/controller/Database.java
@@ -17,7 +17,7 @@ public final class Database extends DBUtil {
     private String TableName = new String();
     private static final String URL = "machexpress";
     private static final String UserName = "root";
-    private static final String Password = "";
+    private static final String Password = "agnimitra";
 
     public Database(String TableName) {
 
--- products/MachExpress/trunk/src/main/java/controller/MachExpress.java
+++ products/MachExpress/trunk/src/main/java/controller/MachExpress.java
@@ -1,48 +1,47 @@
-
-
 package controller;
 
-import view.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.Window;
 
-import java.io.*;
-import java.util.*;
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
+import view.MachExpressFrame;
 
 /**
- *
- * @author  paawak
+ * 
+ * @author paawak
  */
 public class MachExpress {
-    
+
     public MachExpress() {
     }
-    
+
     /**
-     * @param args the command line arguments
+     * @param args
+     *            the command line arguments
      */
-    
-    
-    
+
     public static void main(String[] args) {
-        
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 setLookNFeel();
-                MachExpressFrame frame = MachExpressFrame.INSTANCE;                
-                //centerWindow(frame,1000,900);
+                MachExpressFrame frame = MachExpressFrame.INSTANCE;
+                // centerWindow(frame,1000,900);
                 fullScreen(frame);
                 frame.setVisible(true);
             }
         });
     }
-    
-    /** @param args the command line arguments
-     *
+
+    /**
+     * @param args
+     *            the command line arguments
+     * 
      */
-    public static void centerWindow(javax.swing.JFrame frame, int width, int height) {
+    public static void centerWindow(javax.swing.JFrame frame, int width,
+            int height) {
         Window win = new Window(frame);
         frame.setSize(width, height);
         win.setSize(width, height);
@@ -56,28 +55,37 @@ public class MachExpress {
         }
         int x = (screenDim.width - win.getSize().width) / 2;
         int y = (screenDim.height - win.getSize().height) / 2;
-        frame.setBounds(x, y, width,height);
+        frame.setBounds(x, y, width, height);
     }
-    
-    public static void fullScreen(javax.swing.JFrame frame){
+
+    public static void fullScreen(javax.swing.JFrame frame) {
         Dimension scr = Toolkit.getDefaultToolkit().getScreenSize();
-        
-        frame.setSize(scr.width, scr.height-40);
+
+        frame.setSize(scr.width, scr.height - 40);
     }
-    
-    public static void setLookNFeel(){
-        
+
+    public static void setLookNFeel() {
+
         try {
-            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel" );
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel" );
-            //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            //UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel" );//*/
-            
-        } catch (Exception e) { }
-        
-        
+
+            System.out.println("### MachExpress.setLookNFeel() using "
+                    + System.getProperty("java.home"));
+            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel"
+            // );
+            UIManager
+                    .setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()
+            // );
+            // UIManager.setLookAndFeel(UIManager.
+            // getCrossPlatformLookAndFeelClassName());
+            // UIManager.setLookAndFeel(
+            // "com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+            // UIManager.setLookAndFeel(
+            // "com.sun.java.swing.plaf.windows.WindowsLookAndFeel" );//*/
+
+        } catch (Exception e) {
+        }
+
     }
-    
+
 }

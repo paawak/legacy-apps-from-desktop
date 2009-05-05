42283d075900b7b28de709b3047d058089eb5f02
paawak <paawak@gmail.com>
2009-05-05 20:15:27 +0000

    git-svn-id: svn://localhost/swayam@207 c64e1095-0faf-4994-8903-70d657f34d2f

---
+++ products/biz/ims-core/trunk/src/test/java/com/swayam/web/example/service/MockUserDetailsService.java
@@ -0,0 +1,13 @@
+package com.swayam.web.example.service;
+
+import org.springframework.security.userdetails.UserDetails;
+import org.springframework.security.userdetails.UserDetailsService;
+import org.springframework.security.userdetails.UsernameNotFoundException;
+import com.swayam.web.example.model.User;
+import org.springframework.dao.DataAccessException;
+
+public class MockUserDetailsService implements UserDetailsService {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException {
+        return new User("testuser");
+    }
+}

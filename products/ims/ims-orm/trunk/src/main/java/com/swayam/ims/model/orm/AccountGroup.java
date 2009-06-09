fe05784ecd3f5dd9419136539487ac71c3b6beca
paawak <paawak@gmail.com>
2009-06-09 10:13:56 +0000

    a/c group
    git-svn-id: svn://localhost/swayam@329 c64e1095-0faf-4994-8903-70d657f34d2f

---
+++ products/ims/ims-orm/trunk/src/main/java/com/swayam/ims/model/orm/AccountGroup.java
@@ -0,0 +1,86 @@
+/*
+ * AccountGroup.java
+ *
+ * Created on Jun 9, 2009 2:43:59 PM
+ *
+ * Copyright (c) 2002 - 2008 : Swayam Inc.
+ *
+ * P R O P R I E T A R Y & C O N F I D E N T I A L
+ *
+ * The copyright of this document is vested in Swayam Inc. without
+ * whose prior written permission its contents must not be published,
+ * adapted or reproduced in any form or disclosed or
+ * issued to any third party.
+ */
+
+package com.swayam.ims.model.orm;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * 
+ * @author paawak
+ */
+@Entity
+@Table(name = "account_group")
+public class AccountGroup implements Serializable {
+
+    private static final long serialVersionUID = 8651011772980546778L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String name;
+
+    @Column(length = 50)
+    private String description;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "parent")
+    private AccountGroup parent;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public AccountGroup getParent() {
+        return parent;
+    }
+
+    public void setParent(AccountGroup parent) {
+        this.parent = parent;
+    }
+
+}

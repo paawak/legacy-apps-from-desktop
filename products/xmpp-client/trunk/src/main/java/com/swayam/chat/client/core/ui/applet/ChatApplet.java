479d8e24dd1ea89227ffcdebb692619ddae7de7f
paawak <paawak@gmail.com>
2009-12-06 15:57:39 +0000

    git-svn-id: svn://localhost/swayam@477 c64e1095-0faf-4994-8903-70d657f34d2f

---
+++ products/xmpp-client/trunk/src/main/java/com/swayam/chat/client/core/ui/applet/ChatApplet.java
@@ -0,0 +1,71 @@
+/*
+ * ChatApplet.java
+ *
+ * Created on Dec 6, 2009 9:19:01 PM
+ *
+ * Copyright (c) 2002 - 2009 : Swayam Inc.
+ *
+ * All rights reserved. Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.swayam.chat.client.core.ui.applet;
+
+import javax.swing.JApplet;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+
+/**
+ * 
+ * @author paawak
+ */
+public class ChatApplet extends JApplet {
+
+    private static final long serialVersionUID = -2126075630790344759L;
+
+    public void init() {
+        try {
+            java.awt.EventQueue.invokeAndWait(new Runnable() {
+                public void run() {
+                    initComponents();
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void initComponents() {
+
+        JMenuBar mainMenuBar = new JMenuBar();
+        JMenu buddiesMenu = new JMenu();
+        buddiesMenu.setText("Buddies");
+        JMenuItem addBuddyItem = new JMenuItem();
+        addBuddyItem.setText("Add Buddy");
+        buddiesMenu.add(addBuddyItem);
+        mainMenuBar.add(buddiesMenu);
+
+        setJMenuBar(mainMenuBar);
+
+        JScrollPane centerScrollPane = new JScrollPane();
+        JTree friendsListTree = new JTree();
+
+        centerScrollPane.setViewportView(friendsListTree);
+
+        getContentPane().add(centerScrollPane, java.awt.BorderLayout.CENTER);
+
+    }
+
+}

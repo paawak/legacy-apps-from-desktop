fcef2ad481798d10e27fc01468ca8e0ba632d19b
paawak <paawak@gmail.com>
2010-11-25 18:59:15 +0000

    git-svn-id: svn://localhost/swayam@805 c64e1095-0faf-4994-8903-70d657f34d2f

---
+++ sandbox/HibernateTest/trunk/src/main/java/com/swayam/demo/oracle/hibernate/Semester.java
@@ -0,0 +1,24 @@
+/*
+ * Semester.java
+ *
+ * Created on Nov 26, 2010 12:25:36 AM
+ *
+ * Copyright (c) 2002 - 2008 : Swayam Inc.
+ *
+ * P R O P R I E T A R Y & C O N F I D E N T I A L
+ *
+ * The copyright of this document is vested in Swayam Inc. without
+ * whose prior written permission its contents must not be published,
+ * adapted or reproduced in any form or disclosed or
+ * issued to any third party.
+ */
+
+package com.swayam.demo.oracle.hibernate;
+
+/**
+ *  
+ * @author paawak
+ */
+public class Semester {
+
+}
---
+++ sandbox/HibernateTest/trunk/src/test/java/com/swayam/demo/oracle/hibernate/test/SemesterInsertTest.java
@@ -0,0 +1,50 @@
+/*
+ * CustomTestTest.java
+ *
+ * Created on Oct 31, 2010 5:33:27 PM
+ *
+ * Copyright (c) 2002 - 2008 : Swayam Inc.
+ *
+ * P R O P R I E T A R Y & C O N F I D E N T I A L
+ *
+ * The copyright of this document is vested in Swayam Inc. without
+ * whose prior written permission its contents must not be published,
+ * adapted or reproduced in any form or disclosed or
+ * issued to any third party.
+ */
+
+package com.swayam.demo.oracle.hibernate.test;
+
+import org.hibernate.Session;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.swayam.demo.oracle.hibernate.HibernateUtil;
+
+/**
+ * 
+ * @author paawak
+ */
+public class SemesterInsertTest {
+
+    private Session session;
+
+    @Before
+    public void init() {
+        session = HibernateUtil.getSession();
+    }
+
+    @Test
+    public void insert() {
+
+        // CustomDemo ct = new CustomDemo();
+        // ct.setName("nonameo");
+        // ct.setTimeWithZone(new GregorianCalendar());
+        //
+        // Transaction tr = session.beginTransaction();
+        // session.save(ct);
+        // tr.commit();
+
+    }
+
+}

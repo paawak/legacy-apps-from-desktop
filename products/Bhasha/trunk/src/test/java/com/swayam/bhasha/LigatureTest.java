6b52991e214dbdd426cf1a71f3ee873efb5f9d1c
paawak <paawak@gmail.com>
2012-12-02 11:59:40 +0000

    git-svn-id: svn://localhost/swayam@841 c64e1095-0faf-4994-8903-70d657f34d2f

--- products/Bhasha/trunk/src/test/java/com/swayam/bhasha/LigatureTest.java
+++ products/Bhasha/trunk/src/test/java/com/swayam/bhasha/LigatureTest.java
@@ -3,9 +3,9 @@ package com.swayam.bhasha;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.font.FontRenderContext;
-import java.awt.font.GlyphMetrics;
 import java.awt.font.GlyphVector;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.IOException;
 
@@ -13,16 +13,31 @@ import org.junit.Test;
 
 public class LigatureTest {
 
+    private static final String BANGLA_TEXT = "ক্ষ";
+
     @Test
     public void testApp() throws FontFormatException, IOException {
-        // Font font = new Font("Lohit Bengali", Font.PLAIN, 30);
-        Font font = Font.createFont(Font.TRUETYPE_FONT, new File("/usr/share/fonts/lohit-bengali/Lohit-Bengali.ttf"));
+        Font font = Font.createFont(Font.TRUETYPE_FONT, new File("/usr/share/fonts/lohit-bengali/Lohit-Bengali.ttf")).deriveFont(40f);
         FontRenderContext context = new FontRenderContext(new AffineTransform(), true, true);
-        GlyphVector glyphVector = font.createGlyphVector(context, "ক্ষী");
-        System.out.println(glyphVector.getNumGlyphs());
-        for (int i = 0; i < glyphVector.getNumGlyphs(); i++) {
-            GlyphMetrics metrics = glyphVector.getGlyphMetrics(glyphVector.getGlyphCharIndex(i));
-            System.out.println(i + " : " + metrics.isLigature());
+        GlyphVector glyphVector = font.layoutGlyphVector(context, BANGLA_TEXT.toCharArray(), 0, BANGLA_TEXT.length(), Font.LAYOUT_LEFT_TO_RIGHT);
+
+        int glyphNum = glyphVector.getNumGlyphs();
+
+        float[] glyphPositions = glyphVector.getGlyphPositions(0, glyphNum, null);
+
+        for (float pos : glyphPositions) {
+            System.out.println(pos);
+        }
+
+        Rectangle2D rect = glyphVector.getLogicalBounds();
+        System.out.println(rect);
+
+        int[] charIndices = glyphVector.getGlyphCharIndices(0, glyphNum, null);
+
+        System.out.println("************************");
+
+        for (int charIndex : charIndices) {
+            System.out.println(charIndex);
         }
     }
 }
